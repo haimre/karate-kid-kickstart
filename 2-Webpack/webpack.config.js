3142541e1fa81433ee861e3d5a8baa3388dcd60e
haimre <haimre@wix.com>
2022-02-09 17:37:23 +0200

    second fix: added webpack to 3-JSS dir

--- 2-Webpack/webpack.config.js
+++ 2-Webpack/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    devtool: 'eval-source-map',
     mode: 'development',
     entry:{
         main: path.resolve(__dirname,'src/index.js'),
