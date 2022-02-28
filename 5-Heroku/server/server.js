5b3535fe369e86dcbb5a709826c9ead00a966a2f
haimre <haimre@wix.com>
2022-02-28 15:19:04 +0200

    heroku setup

--- 5-Heroku/server/server.js
+++ 5-Heroku/server/server.js
@@ -3,7 +3,6 @@ const storageRouter = require('./routes/storage')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const mongodb = require('mongodb')
 const dotenv = require('dotenv')
 const IDinCookie = require('./middleware/IDinCookie')
 dotenv.config({ path: '.env' })
@@ -12,12 +11,11 @@ require('./db/connect')
 
 
 const app = express()
-app.use(cors({ origin: true, credentials: true }));
+app.use(express.static(`${__dirname}/public`))
 app.use(bodyParser.json());
 app.use(cookieParser())
-
 app.use(IDinCookie)
 
-app.use('/storage', storageRouter)
+app.use('/todos', todosRouter)
 
 app.listen(process.env.SERVER_PORT)
